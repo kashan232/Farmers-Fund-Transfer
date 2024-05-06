5de820a961dfec457e245b1aa4e7626326c4c0b3
Kashan Shaikh <shaikhkashan670@gmail.com>
2024-05-06 12:29:45 +0500

    done

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ use App\Models\LeaveRequest;
 use Illuminate\Support\Facades\Route;
 
 /*
+// ap naraz he?? Aiman Here
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
