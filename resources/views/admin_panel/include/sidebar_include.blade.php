024436c6e0a036dd8a4586201320b0aab6e1c1fc
Kashan Shaikh <shaikhkashan670@gmail.com>
2024-06-08 15:56:45 +0500

    change

--- resources/views/admin_panel/include/sidebar_include.blade.php
+++ resources/views/admin_panel/include/sidebar_include.blade.php
@@ -1,76 +1,81 @@
-<div class="dlabnav">
-    <div class="dlabnav-scroll">
-        <ul class="metismenu" id="menu">
-            <li>
-                <a class="" href="{{ route('home') }}" aria-expanded="false">
-                    <i class="fa-solid fa-gear fw-bold"></i>
-                    <span class="nav-text">Dasboard</span>
+<div class="navbar-wrapper">
+            <div class="m-header">
+                <a href="index.html" class="b-brand text-primary">
+                    <img src="../assets/images/logo-dark.svg" alt="logo image" class="logo-lg" />
                 </a>
+            </div>
+            <div class="navbar-content">
+                <ul class="pc-navbar">
+                    <li class="pc-item pc-hasmenu">
+                        <a href="{{ route('home') }}" class="pc-link">
+                            <span class="pc-micon">
+                                <i class="ph-duotone ph-gauge"></i>
+                            </span>
+                            <span class="pc-mtext">Dashboard</span>
+                        </a>
+                    </li>
 
-            </li>
-            <li>
-                <a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
-                    <i class="fas fa-globe-europe"></i>
-                    <span class="nav-text">District</span>
-                </a>
-                <ul aria-expanded="false">
-                    <li><a href="{{ route('add-district') }}">Add</a></li>
-                    <li><a href="{{ route('all-district') }}">List</a></li>
-                </ul>
+                    <!-- <li class="pc-item">
+                        <a href="#" class="pc-link">
+                            <span class="pc-micon">
+                                <i class="ph-duotone ph-chart-pie"></i>
+                            </span>
+                            <span class="pc-mtext">Chart</span>
+                        </a>
+                    </li> -->
+                    <li class="pc-item pc-hasmenu">
+                        <a href="#!" class="pc-link">
+                            <span class="pc-micon">
+                                <i class="ph-duotone ph-globe"></i>
+                            </span>
+                            <span class="pc-mtext">District</span>
+                            <span class="pc-arrow"><i data-feather="chevron-right"></i></span>
+                        </a>
+                        <ul class="pc-submenu">
+                            <li class="pc-item">
+                                <a class="pc-link" href="{{ route('add-district') }}">Add</a>
+                            </li>
+                            <li class="pc-item">
+                                <a class="pc-link" href="{{ route('all-district') }}">List</a>
+                            </li>
+                        </ul>
+                    </li>
 
-            </li>
-            <li>
-                <a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
-                    <i class="fas fa-city"></i>
-                    <span class="nav-text">Tehsil</span>
-                </a>
-                <ul aria-expanded="false">
-                    <li><a href="{{ route('add-tehsil') }}">Add</a></li>
-                    <li><a href="{{ route('all-tehsil') }}">List</a></li>
-                </ul>
+                    <li class="pc-item pc-hasmenu">
+                        <a href="#!" class="pc-link">
+                        <span class="pc-micon">
+                                <i class="ph-duotone ph-buildings"></i>
+                            </span>
+                            <span class="pc-mtext">Tehsil</span>
+                            <span class="pc-arrow"><i data-feather="chevron-right"></i></span>
+                        </a>
+                        <ul class="pc-submenu">
+                            <li class="pc-item">
+                                <a class="pc-link" href="{{ route('add-tehsil') }}">Add</a>
+                            </li>
+                            <li class="pc-item">
+                                <a class="pc-link" href="{{ route('all-tehsil') }}">List</a>
+                            </li>
+                        </ul>
+                    </li>
 
-            </li>
-            <li>
-                <a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
-                    <i class="fas fa-building"></i>
-                    <span class="nav-text">Area</span>
-                </a>
-                <ul aria-expanded="false">
-                    <li><a href="{{ route('add-area') }}">Add</a></li>
-                    <li><a href="{{ route('all-area') }}">List</a></li>
-                </ul>
-
-            </li>
-            <li>
-                <a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
-                    <i class="fas fa-boxes"></i>
-                    <span class="nav-text">Tappa</span>
-                </a>
-                <ul aria-expanded="false">
-                    <li><a href="{{ route('add-tappa') }}">Add</a></li>
-                    <li><a href="{{ route('all-tappa') }}">List</a></li>
-                </ul>
-            </li>
-            <li>
-                <a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
-                    <i class="fas fa-user-plus"></i>
-                    <span class="nav-text">Agriculture</span>
-                </a>
-                <ul aria-expanded="false">
-                    <li><a href="{{ route('agri-officer-create') }}">Create Officer</a></li>
-                    <li><a href="{{ route('all-agri-officer') }}">All Officer</a></li>
-                </ul>
-            </li>
-            <li>
-                <a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
-                    <i class="fas fa-user-plus"></i>
-                    <span class="nav-text">Land Revenue</span>
-                </a>
-                <ul aria-expanded="false">
-                    <li><a href="{{ route('add-revenue-officer') }}">Create Officer</a></li>
-                    <li><a href="{{ route('all-revenue-officer') }}">All Officer</a></li>
-                </ul>
-            </li>
+                    <li class="pc-item pc-hasmenu">
+                        <a href="#!" class="pc-link">
+                            <span class="pc-micon">
+                                <i class="ph-duotone ph-buildings"></i>
+                            </span>
+                            <span class="pc-mtext">Area</span>
+                            <span class="pc-arrow"><i data-feather="chevron-right"></i></span>
+                        </a>
+                        <ul class="pc-submenu">
+                            <li class="pc-item">
+                                <a class="pc-link" href="{{ route('add-area') }}">Add</a>
+                            </li>
+                            <li class="pc-item">
+                                <a class="pc-link" href="{{ route('all-area') }}">List</a>
+                            </li>
+                        </ul>
+                    </li>
 
                     <li class="pc-item pc-hasmenu">
                         <a href="#!" class="pc-link">
@@ -86,6 +91,24 @@
                             </li>
                             <li class="pc-item">
                                 <a class="pc-link" href="{{ route('all-tappa') }}">List</a>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li class="pc-item pc-hasmenu">
+                        <a href="#!" class="pc-link">
+                        <span class="pc-micon">
+                                <i class="ph-duotone ph-buildings"></i>
+                            </span>
+                            <span class="pc-mtext">UC</span>
+                            <span class="pc-arrow"><i data-feather="chevron-right"></i></span>
+                        </a>
+                        <ul class="pc-submenu">
+                            <li class="pc-item">
+                                <a class="pc-link" href="{{ route('add-uc') }}">Add</a>
+                            </li>
+                            <li class="pc-item">
+                                <a class="pc-link" href="{{ route('all-uc') }}">List</a>
                             </li>
                         </ul>
                     </li>
