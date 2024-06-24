701bfc1d9947a8e1ea72f26aab20c8450c46e163
Kashan Shaikh <shaikhkashan670@gmail.com>
2024-06-24 16:30:32 +0500

    changes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,8 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AgricultureOfficer;
+use App\Models\AgriUser;
+use App\Models\District;
+use App\Models\LandRevenueDepartment;
+use App\Models\Tappa;
+use App\Models\Tehsil;
+use App\Models\UC;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -19,7 +27,112 @@ class HomeController extends Controller
             }
             else if($usertype=='admin')
             {
-                return view('admin_panel.admin_dashboard');
+                $expirationInSeconds = 3600;
+
+                $district_counts = cache()->remember('district_counts', $expirationInSeconds, function () {
+                    return District::count();
+                });
+                $tehsil_counts = cache()->remember('tehsil_counts', $expirationInSeconds, function () {
+                    return Tehsil::count();
+                });
+                $ucs_counts = cache()->remember('ucs_counts', $expirationInSeconds, function () {
+                    return UC::count();
+                });
+                $tappas_count = cache()->remember('tappas_count', $expirationInSeconds, function () {
+                    return Tappa::count();
+                });
+
+                $AgriUser = cache()->remember('AgriUser', $expirationInSeconds, function () {
+                    return AgriUser::count();
+                });
+
+                $AgricultureOfficer = cache()->remember('AgricultureOfficer', $expirationInSeconds, function () {
+                    return AgricultureOfficer::count();
+                });
+
+                $LandRevenueDepartment = cache()->remember('LandRevenueDepartment', $expirationInSeconds, function () {
+                    return LandRevenueDepartment::count();
+                });
+                
+                $agriUserCount = DB::table('agriculture_user_farmer_registrations')->count();
+                $agricultureOfficerCount = DB::table('agriculture_farmers_registrations')->count();
+                $landRevenueDepartmentCount = DB::table('land_revenue_farmer_registations')->count();
+                $onlineFarmerRegistrationCount = DB::table('online_farmer_registrations')->count();
+            
+                
+                $totalEntries = $agriUserCount + $agricultureOfficerCount + $landRevenueDepartmentCount + $onlineFarmerRegistrationCount;
+            
+                   
+                $Unverifiedfarmeragiruser = DB::table('agriculture_user_farmer_registrations')->where('verification_status', '=', 'Unverified')->count();
+                $Unverifiedfarmeragioficr = DB::table('agriculture_farmers_registrations')->where('verification_status', '=', 'Unverified')->count();
+                $Unverifiedfarmerland = DB::table('land_revenue_farmer_registations')->where('verification_status', '=', 'Unverified')->count();
+                $Unverifiedfarmeronline = DB::table('online_farmer_registrations')->where('verification_status', '=', 'Unverified')->count();
+
+                
+                $TotalUnverifiedfarmer = $Unverifiedfarmeragiruser + $Unverifiedfarmeragioficr + $Unverifiedfarmerland + $Unverifiedfarmeronline;
+
+                // dd($totalEntries);
+                $data = cache()->remember('data', $expirationInSeconds, function () {
+                    return DB::table('districts')
+                        ->leftJoin('agriculture_farmers_registrations', 'agriculture_farmers_registrations.district', '=', 'districts.district')
+                        ->select('districts.district', DB::raw('COUNT(agriculture_farmers_registrations.id) as user_count'))
+                        ->groupBy('districts.district')
+                        ->get()
+                        ->map(function ($item) {
+                            return [
+                                'label' => $item->district,
+                                'value' => $item->user_count,
+                            ];
+                        })
+                        ->toArray();
+                });
+
+                $data2 = cache()->remember('data2', $expirationInSeconds, function () {
+                    return DB::table('districts')
+                        ->leftJoin('land_revenue_farmer_registations', 'land_revenue_farmer_registations.district', '=', 'districts.district')
+                        ->select('districts.district', DB::raw('COUNT(land_revenue_farmer_registations.id) as user_count'))
+                        ->groupBy('districts.district')
+                        ->get()
+                        ->map(function ($item) {
+                            return [
+                                'label' => $item->district,
+                                'value' => $item->user_count,
+                            ];
+                        })
+                        ->toArray();
+                });
+
+                $data3 = cache()->remember('data3', $expirationInSeconds, function () {
+                    return DB::table('districts')
+                        ->leftJoin('online_farmer_registrations', 'online_farmer_registrations.district', '=', 'districts.district')
+                        ->select('districts.district', DB::raw('COUNT(online_farmer_registrations.id) as user_count'))
+                        ->groupBy('districts.district')
+                        ->get()
+                        ->map(function ($item) {
+                            return [
+                                'label' => $item->district,
+                                'value' => $item->user_count,
+                            ];
+                        })
+                        ->toArray();
+                });
+
+                // dd($data3);
+                return view('admin_panel.admin_dashboard', [
+                    'data' => $data,
+                    'data2' => $data2,
+                    'data3' => $data3,
+                    'district_counts' => $district_counts,
+                    'tehsil_counts' => $tehsil_counts,
+                    'ucs_counts' => $ucs_counts,
+                    'tappas_count' => $tappas_count,
+                    'AgriUser' => $AgriUser,
+                    'AgricultureOfficer' => $AgricultureOfficer,
+                    'LandRevenueDepartment' => $LandRevenueDepartment,
+                    'totalEntries' => $totalEntries,
+                    'TotalUnverifiedfarmer' => $TotalUnverifiedfarmer,
+                    
+                ]);
             }
             else if($usertype=='Agriculture_Officer')
             {
---
+++ resources/views/land_revenue_panel/online_farmers_verifications/unverify_farmers_by_land.blade.php
@@ -0,0 +1,188 @@
+@include('land_revenue_panel.include.header_include')
+<!-- [ Pre-loader ] End -->
+<!-- [ Sidebar Menu ] start -->
+<nav class="pc-sidebar">
+    @include('land_revenue_panel.include.sidebar_include')
+</nav>
+
+<!-- [ Sidebar Menu ] end -->
+<!-- [ Header Topbar ] start -->
+<header class="pc-header">
+    @include('land_revenue_panel.include.navbar_include')
+</header>
+<!-- [ Header ] end -->
+
+
+<div id="exampleModalLive" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="exampleModalLiveLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLiveLabel">Farmers Verification</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="container">
+                    <div class="row">
+                        <form id="verifyfarmers" action="{{ route('verify-unverify-online-farmers') }}" method="POST">
+                            @csrf
+                            <div class="form-group">
+                                <label for="statusSelect">Status</label>
+                                <input type="hidden" id="farmers_id" name="farmers_id" readonly>
+                                <select class="form-control" id="statusSelect" name="verification_status">
+                                    <option value="Verified">Verified</option>
+                                    <option value="Unverified">Unverified</option>
+                                </select>
+                            </div>
+                            <div class="form-group" id="reasonBox" style="display: none;">
+                                <label for="reasonTextarea">Reason</label>
+                                <textarea class="form-control" id="reasonTextarea" name="declined_reason" rows="3"></textarea>
+                            </div>
+                            <button type="submit" class="btn btn-primary mt-3">Save</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!-- [ Main Content ] start -->
+<div class="pc-container">
+    <div class="pc-content">
+        <!-- [ breadcrumb ] start -->
+        <div class="page-header">
+            <div class="page-block">
+                <div class="row align-items-center">
+                    <div class="col-md-12">
+                        <div class="page-header-title">
+                            <h2 class="mb-0">Online Unverify Farmers</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- [ breadcrumb ] end -->
+        <!-- [ Main Content ] start -->
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="dt-responsive">
+                            <div id="dom-jqry_wrapper" class="dt-container dt-bootstrap5">
+                                <div class="row mt-2 justify-content-between">
+                                    <div class="col-md-auto me-auto ">
+                                        <div class="dt-length"><select name="dom-jqry_length" aria-controls="dom-jqry" class="form-select form-select-sm" id="dt-length-0">
+                                                <option value="10">10</option>
+                                                <option value="25">25</option>
+                                                <option value="50">50</option>
+                                                <option value="100">100</option>
+                                            </select></div>
+                                    </div>
+                                    <div class="col-md-auto ms-auto ">
+                                        <div class="dt-search"><label for="dt-search-0">Search:</label><input type="search" class="form-control form-control-sm" id="dt-search-0" placeholder="" aria-controls="dom-jqry"></div>
+                                    </div>
+                                </div>
+                                <div class="row mt-2 justify-content-md-center">
+                                    <div class="col-12 ">
+                                        <div class="table-responsive">
+                                            <table id="dom-jqry" class="table table-striped table-bordered nowrap dataTable" aria-describedby="dom-jqry_info" style="width: 100%;">
+                                                <thead>
+                                                    <tr>
+                                                        <th>Name</th>
+                                                        <th>CNIC</th>
+                                                        <th>District</th>
+                                                        <th>Tehsil</th>
+                                                        <th>UC</th>
+                                                        <th>Area</th>
+                                                        <th>Mobile</th>
+                                                        <th>Status</th>
+                                                        <th>Verify</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    @foreach($online_farmers as $online_farmer)
+                                                    <tr>
+                                                        <td>{{ $online_farmer->name }}</td>
+                                                        <td>{{ $online_farmer->cnic }}</td>
+                                                        <td>{{ $online_farmer->district }}</td>
+                                                        <td>{{ $online_farmer->tehsil }}</td>
+                                                        <td>{{ $online_farmer->uc }}</td>
+                                                        <td>{{ $online_farmer->area }}</td>
+                                                        <td>{{ $online_farmer->mobile }}</td>
+                                                        <td>
+                                                            @if ($online_farmer->verification_status === 'Verified')
+                                                            <span class="badge text-bg-success">Verified</span>
+                                                            @else
+                                                            <span class="badge text-bg-danger">Unverified</span>
+                                                            @endif
+                                                        </td>
+                                                        <td>
+                                                            <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModalLive" data-id="{{ $online_farmer->id }}">Verify</button>
+                                                        </td>
+                                                    </tr>
+                                                    @endforeach
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row mt-2 justify-content-between">
+                                    <div class="col-md-auto me-auto ">
+                                        <div class="dt-info" aria-live="polite" id="dom-jqry_info" role="status">Showing 1 to 10 of 20 entries</div>
+                                    </div>
+                                    <div class="col-md-auto ms-auto ">
+                                        <div class="dt-paging paging_full_numbers">
+                                            <ul class="pagination">
+                                                <li class="dt-paging-button page-item disabled"><a class="page-link first" aria-controls="dom-jqry" aria-disabled="true" aria-label="First" data-dt-idx="first" tabindex="-1">«</a></li>
+                                                <li class="dt-paging-button page-item disabled"><a class="page-link previous" aria-controls="dom-jqry" aria-disabled="true" aria-label="Previous" data-dt-idx="previous" tabindex="-1">‹</a></li>
+                                                <li class="dt-paging-button page-item active"><a href="#" class="page-link" aria-controls="dom-jqry" aria-current="page" data-dt-idx="0" tabindex="0">1</a></li>
+                                                <li class="dt-paging-button page-item"><a href="#" class="page-link" aria-controls="dom-jqry" data-dt-idx="1" tabindex="0">2</a></li>
+                                                <li class="dt-paging-button page-item"><a href="#" class="page-link next" aria-controls="dom-jqry" aria-label="Next" data-dt-idx="next" tabindex="0">›</a></li>
+                                                <li class="dt-paging-button page-item"><a href="#" class="page-link last" aria-controls="dom-jqry" aria-label="Last" data-dt-idx="last" tabindex="0">»</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- [ Main Content ] end -->
+    </div>
+</div>
+<!-- [ Main Content ] end -->
+<footer class="pc-footer">
+    @include('land_revenue_panel.include.footer_copyright_include')
+</footer>
+
+@include('land_revenue_panel.include.footer_include')
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        // Event listener for opening the modal
+        var exampleModalLive = document.getElementById('exampleModalLive');
+        exampleModalLive.addEventListener('show.bs.modal', function(event) {
+            var button = event.relatedTarget;
+            var farmerId = button.getAttribute('data-id');
+            var farmersIdInput = exampleModalLive.querySelector('#farmers_id');
+            farmersIdInput.value = farmerId;
+        });
+
+        // Event listener for changing the status
+        var statusSelect = document.getElementById('statusSelect');
+        statusSelect.addEventListener('change', function() {
+            var reasonBox = document.getElementById('reasonBox');
+            if (this.value === 'Unverified') {
+                reasonBox.style.display = 'block';
+            } else {
+                reasonBox.style.display = 'none';
+            }
+        });
+    });
+</script>
+
+</body>
+
+</html>
