f2f42b13f02629ff0ab321f65c1e9b4f4c6e125a
Kashan Shaikh <shaikhkashan670@gmail.com>
2024-09-30 11:53:52 +0500

    upload

--- resources/views/admin_panel/complains/complains.blade.php
+++ resources/views/admin_panel/complains/complains.blade.php
@@ -6,9 +6,9 @@
 <!-- [ Header Topbar ] start -->
 @include('admin_panel.include.navbar_include')
 <style>
-    .list-group
-    {
-        padding: 4px 4px!important;
+    .list-group {
+        --bs-list-group-item-padding-x: 10px !important;
+        --bs-list-group-item-padding-y: 6.666667px !important;
     }
 </style>
 <!-- [ Header ] end -->
@@ -27,7 +27,7 @@
                                             <div class="row">
                                                 <div class="col">
                                                     <p class="card-title text-title">Total District</p>
-                                                    <h3 class="card-text text-amount">3333</h3>
+                                                    <h3 class="card-text text-amount">56</h3>
                                                 </div>
                                                 <div class="col-auto">
                                                     <div class="icon-shape green-icon-bg">
@@ -46,7 +46,7 @@
                                             <div class="row">
                                                 <div class="col">
                                                     <p class="card-title text-title"> Total Taluka </p>
-                                                    <h3 class="card-text text-amount">32 / 168</h3>
+                                                    <h3 class="card-text text-amount">168</h3>
                                                 </div>
                                                 <div class="col-auto">
                                                     <div class="icon-shape green-icon-bg">
@@ -66,7 +66,7 @@
                                             <div class="row">
                                                 <div class="col">
                                                     <p class="card-title text-title">Total Complaints</p>
-                                                    <h3 class="card-text text-amount">32 / 168</h3>
+                                                    <h3 class="card-text text-amount">800</h3>
                                                 </div>
                                                 <div class="col-auto">
                                                     <div class="icon-shape green-icon-bg">
@@ -86,7 +86,7 @@
                                             <div class="row">
                                                 <div class="col">
                                                     <p class="card-title text-title">Closed Complaints</p>
-                                                    <h3 class="card-text text-amount">32 / 168</h3>
+                                                    <h3 class="card-text text-amount">400</h3>
                                                 </div>
                                                 <div class="col-auto">
                                                     <div class="icon-shape green-icon-bg">
@@ -106,7 +106,7 @@
                                             <div class="row">
                                                 <div class="col">
                                                     <p class="card-title text-title">In-Progress Complaints</p>
-                                                    <h3 class="card-text text-amount">32 / 168</h3>
+                                                    <h3 class="card-text text-amount">200</h3>
                                                 </div>
                                                 <div class="col-auto">
                                                     <div class="icon-shape green-icon-bg">
@@ -126,7 +126,7 @@
                                             <div class="row">
                                                 <div class="col">
                                                     <p class="card-title text-title">UnResolved Complaints</p>
-                                                    <h3 class="card-text text-amount">32 / 168</h3>
+                                                    <h3 class="card-text text-amount">200</h3>
                                                 </div>
                                                 <div class="col-auto">
                                                     <div class="icon-shape green-icon-bg">
@@ -147,6 +147,13 @@
         </div>
 
         <div class="row">
+
+            <div class="col-md-8">
+                <div class="card">
+                    <canvas id="complaintsChart" width="600" height="500"></canvas>
+                </div>
+            </div>
+
             <div class="col-md-4">
                 <div class="card border-info">
                     <div class="text-center pt-3 pb-3 w-100 bg-dark">
@@ -253,6 +260,55 @@
 
 @include('admin_panel.include.footer_include')
 
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script>
+    var cityLabels = ['Badin', 'Hyderabad', 'Dadu', 'Matiari', 'Umerkot']; // Static city labels
+var complaintData1 = [10, 20, 15, 8, 12]; // Static data for 'Un-Resolved' complaints
+var complaintData2 = [5, 7, 6, 3, 4]; // Static data for 'In-Progress' complaints
+var complaintData3 = [25, 30, 20, 10, 15]; // Static data for 'Closed' complaints
+
+var ctx = document.getElementById('complaintsChart').getContext('2d');
+
+var complaintsChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: cityLabels,
+        datasets: [{
+                label: 'Un-Resolved',
+                data: complaintData1,
+                backgroundColor: 'rgba(49, 178, 67, 1)',
+                borderColor: 'rgb(49, 178, 67, 1)',
+                borderWidth: 1
+            },
+            {
+                label: 'In-Progress',
+                data: complaintData2,
+                backgroundColor: 'rgba(64, 182, 109, 1)',
+                borderColor: 'rgb(64, 182, 109, 1)',
+                borderWidth: 1
+            },
+            {
+                label: 'Closed',
+                data: complaintData3,
+                backgroundColor: 'rgba(73, 143, 100, 1)',
+                borderColor: 'rgb(73, 143, 100, 1)',
+                borderWidth: 1
+            }
+        ]
+    },
+    options: {
+        scales: {
+            x: {
+                type: 'category',
+            },
+            y: {
+                beginAtZero: true,
+            }
+        }
+    }
+});
+
+</script>
 </body>
 
 </html>
