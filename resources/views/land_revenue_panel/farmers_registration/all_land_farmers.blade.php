20e096911f7bda2a728c7cba7a432954fa28c436
Mohammad Azhar <90447167+MohammarAzher@users.noreply.github.com>
2024-11-29 15:03:04 +0500

    updateddd

--- resources/views/land_revenue_panel/farmers_registration/all_land_farmers.blade.php
+++ resources/views/land_revenue_panel/farmers_registration/all_land_farmers.blade.php
@@ -28,7 +28,16 @@
                 <div class="row align-items-center">
                     <div class="col-md-12">
                         <div class="page-header-title">
-                            <h2 class="mb-0">O-F Farmers List</h2>
+                            <h2 class="mb-0">
+                                @if(!empty($all_land_farmers) && isset($all_land_farmers[0]) && $all_land_farmers[0] != null)
+                                @if($all_land_farmers[0]->user_type  == 'Agri_Officer') A-O Farmers list
+                                @elseif($all_land_farmers[0]->user_type  == 'Online') Online Farmers List
+                                @else
+                                F-A Farmers list
+                                @endif
+                                Farmers List
+                                @endif
+                            </h2>
                         </div>
                     </div>
                 </div>
