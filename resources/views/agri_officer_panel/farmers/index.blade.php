57ee8acecf52282982301b2cb30581b86defd72f
Mohammad Azhar <90447167+MohammarAzher@users.noreply.github.com>
2024-11-29 14:48:23 +0500

    hoo

--- resources/views/agri_officer_panel/farmers/index.blade.php
+++ resources/views/agri_officer_panel/farmers/index.blade.php
@@ -68,7 +68,7 @@
                     <div class="col-md-12">
                         <div class="page-header-title">
                             <h2 class="mb-0">
-                                @if(!empty($farmers))
+                                @if(count($farmers) > 0)
                                 @if($farmers[0]->user_type  == 'Agri_Officer') A-O Farmers list
                                 @elseif($farmers[0]->user_type  == 'Online') Online Farmers List
                                 @else
