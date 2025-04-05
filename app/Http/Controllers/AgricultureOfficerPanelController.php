4b6b642dedc6926cfb493eac34ef2185a512e0c3
Mohammad Azhar <90447167+MohammarAzher@users.noreply.github.com>
2025-04-05 13:14:24 +0500

    up

--- app/Http/Controllers/AgricultureOfficerPanelController.php
+++ app/Http/Controllers/AgricultureOfficerPanelController.php
@@ -290,12 +290,12 @@ class AgricultureOfficerPanelController extends Controller
         $farmers = LandRevenueFarmerRegistation::where('district', $user->district)
         ->whereIn('tehsil', json_decode($user->tehsil))
         ->whereIn('tappa', json_decode($user->tappas))
-        ->where(function($query) {
-            $query->where('verification_status', 'rejected_by_lrd')
-            ->orWhere('verification_status', 'verified_by_fa');
+        // ->where(function($query) {
+        //     $query->where('verification_status', 'rejected_by_lrd')
+        //     ->orWhere('verification_status', 'verified_by_fa');
 
-                //   ->orWhere('verification_status', null);
-        })
+        //         //   ->orWhere('verification_status', null);
+        // })
         ->paginate(10);
 
 
