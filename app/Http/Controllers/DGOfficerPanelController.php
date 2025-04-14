a0ec6c740082f1c5c7ecb03f99ae76d4d9f57578
Mohammad Azhar <90447167+MohammarAzher@users.noreply.github.com>
2025-04-14 17:31:20 +0500

    up

--- app/Http/Controllers/DGOfficerPanelController.php
+++ app/Http/Controllers/DGOfficerPanelController.php
@@ -143,12 +143,13 @@ class DGOfficerPanelController extends Controller
 
 
             $district = $req->district; // e.g., "Badin"
+dd($req->district, json_encode($req->district));
 
             $agriUsers = User::select('id', 'name', 'number', 'cnic', 'email', 'district', 'tehsil', 'tappas')
                 ->whereRaw('JSON_CONTAINS(district, ?)', [json_encode($district)]) // becomes '"Badin"'
                 ->where('usertype', 'DD_Officer')
                 ->get();
-            
+
 
 
 
