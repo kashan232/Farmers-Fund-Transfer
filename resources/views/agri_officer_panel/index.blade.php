7ae7c25c4de5a94398d06177489f37a8fcd66055
Mohammad Azhar <90447167+MohammarAzher@users.noreply.github.com>
2025-04-29 11:17:20 +0500

    up

--- resources/views/agri_officer_panel/index.blade.php
+++ resources/views/agri_officer_panel/index.blade.php
@@ -128,7 +128,7 @@
                                 </div>
                             </div>
 
-                            <div class="col-lg-12 col-md-6 col-sm-6">
+                            <div class="col-lg-4 col-md-6 col-sm-6">
                                 <div class="card">
                                     <div class="card-body">
                                         <div class="row">
