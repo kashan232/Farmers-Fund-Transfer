3151a5d55e3047683c06315d5ef25e103b422315
Mohammad Azhar <90447167+MohammarAzher@users.noreply.github.com>
2025-04-29 09:56:49 +0500

    up

--- resources/views/field_officer_panel/farmers/index.blade.php
+++ resources/views/field_officer_panel/farmers/index.blade.php
@@ -94,14 +94,14 @@
                                                             <span class="badge  text-bg-success text-dark font-weight-bold" >Forward to DD</span>
                                                             @elseif($farmer->verification_status == 'verified_by_dd')
                                                             <span class="badge  text-bg-success text-dark font-weight-bold" >Forward to LRD</span>
-                                                            
+
                                                             @elseif($farmer->verification_status == 'rejected_by_ao')
                                                             <span class="badge text-bg-danger text-dark font-weight-bold">Rejected By AO</span>
                                                             @elseif($farmer->verification_status == 'rejected_by_dd')
                                                             <span class="badge text-bg-danger text-dark font-weight-bold">Rejected By DD</span>
                                                             @elseif($farmer->verification_status == 'rejected_by_lrd')
                                                             <span class="badge text-bg-danger text-dark font-weight-bold">Rejected By LRD</span>
-                                                            
+
                                                             @else
                                                             <span class="badge text-bg-primary text-white font-weight-bold">Unverified</span>
                                                             @endif
@@ -114,15 +114,17 @@
                                                         <td></td>
                                                         @endif
                                                         <td>
+                                                            @if($farmer->verification_status != 'verified_by_fa' || $farmer->verification_status != 'verified_by_ao' || $farmer->verification_status != 'verified_by_dd' || $farmer->verification_status != 'verified_by_lrd')
                                                             <div class="d-flex">
                                                                 {{-- <a href="{{ route('farmer-view-by-field-officer', ['id' => $farmer->id]) }}" class="btn btn-success btn-sm"><i class="fas fa-eye"></i></a>&nbsp; --}}
 
-                                                                @if ($farmer->verification_status != 'verified_by_lrd')
+
                                                                 <a href="{{ route('farmer-edit-by-field-officer', ['id' => $farmer->id]) }}" class="btn  btn-primary btn-sm"><i class="fas fa-pencil-alt"></i></a>&nbsp;
-                                                                @endif
+
 
                                                                 <a href="{{route('view-farmers-by-field-officer',$farmer->id)}}" class="btn btn-primary btn-sm"><i class="fas fa-eye"></i></a>
                                                             </div>
+                                                            @endif
                                                         </td>
                                                     </tr>
                                                     @endforeach
