2cce82d93aacbad2875fe7cebbff7d7af1ed7299
Mohammad Azhar <90447167+MohammarAzher@users.noreply.github.com>
2025-05-05 08:23:53 +0500

    up

--- resources/views/field_officer_panel/farmers/create.blade.php
+++ resources/views/field_officer_panel/farmers/create.blade.php
@@ -331,11 +331,11 @@
 
                                         <div class="mb-6 col-md-2 py-2">
                                             <label class="form-label">CNIC Issue Date.: <span class="text-danger">*</span></label>
-                                            <input type="text" id="cnic_issue_date" name="cnic_issue_date" class="form-control" value="{{ $data->cnic_issue_date ? \Carbon\Carbon::parse($data->cnic_issue_date)->format('d-m-Y') : '' }}"   data-inputmask="'mask': '99-99-9999'" placeholder="DD-MM-YYYY"   >
+                                            <input type="text" id="cnic_issue_date" name="cnic_issue_date" class="form-control" value="@if(isset($data)) {{ $data->cnic_issue_date ? \Carbon\Carbon::parse($data->cnic_issue_date)->format('d-m-Y') : '' }} @endif"   data-inputmask="'mask': '99-99-9999'" placeholder="DD-MM-YYYY"   >
                                         </div>
                                         <div class="mb-6 col-md-2 py-2 cnic_expiry_date_div">
                                             <label class="form-label">CNIC Expiry Date.: <span class="text-danger">*</span></label>
-                                            <input type="text" id="cnic_expiry_date" name="cnic_expiry_date" class="form-control" value="{{ $data->cnic_expiry_date ? \Carbon\Carbon::parse($data->cnic_expiry_date)->format('d-m-Y') : '' }}"   data-inputmask="'mask': '99-99-9999'" placeholder="DD-MM-YYYY"   >
+                                            <input type="text" id="cnic_expiry_date" name="cnic_expiry_date" class="form-control" value="@if(isset($data))  {{ $data->cnic_expiry_date ? \Carbon\Carbon::parse($data->cnic_expiry_date)->format('d-m-Y') : '' }} @endif"   data-inputmask="'mask': '99-99-9999'" placeholder="DD-MM-YYYY"   >
                                         </div>
                                         <div class="mb-6 col-md-6 py-2">
                                             <label class="form-label">Q4. Mobile No.: <span class="text-danger">*</span></label>
@@ -902,7 +902,7 @@
 
                                         <div class="mb-6 col-md-4 mt-2">
                                             <label class="form-label">City <span class="text-danger">*</span></label>
-                                            <select name="city" id="city" class="form-control js-example-basic-single-no-tag"> 
+                                            <select name="city" id="city" class="form-control js-example-basic-single-no-tag">
                                                 <option value="">Select City</option>
                                                 @foreach ($cities as $city)
                                                     <option value="{{$city->id}}" @if(isset($data)) {{($data->city == $city->id) ? 'selected':''}} @endif>{{$city->title}}</option>
@@ -925,7 +925,7 @@
 
                                         <div class="mb-6 col-md-4 mt-2">
                                             <label class="form-label">Date of Birth (D-M-Y) <span class="text-danger">*</span></label>
-                                            <input type="text" name="date_of_birth" data-inputmask="'mask': '99-99-9999'" placeholder="DD-MM-YYYY" id="date_of_birth" class="form-control" value="{{ $data->date_of_birth ? \Carbon\Carbon::parse($data->date_of_birth)->format('d-m-Y') : '' }}" >
+                                            <input type="text" name="date_of_birth" data-inputmask="'mask': '99-99-9999'" placeholder="DD-MM-YYYY" id="date_of_birth" class="form-control" value="@if(isset($data)) {{ $data->date_of_birth ? \Carbon\Carbon::parse($data->date_of_birth)->format('d-m-Y') : '' }} @endif" >
                                         </div>
 
                                         <div class="mb-6 col-md-4 mt-2">
