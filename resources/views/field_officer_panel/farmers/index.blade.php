5b6b35f153498d9f9a53cb7b76b626468b940661
Mohammad Azhar <90447167+MohammarAzher@users.noreply.github.com>
2025-05-09 13:22:10 +0500

    up

--- resources/views/field_officer_panel/farmers/index.blade.php
+++ resources/views/field_officer_panel/farmers/index.blade.php
@@ -156,7 +156,7 @@
 
         table = $('#example1').DataTable({
             "pageLength": 100, // Default number of rows per page
-            "dom": 'frt', // Only include the filter (search box), table, and pagination
+            "dom": 'lfrtip', // Only include the filter (search box), table, and pagination
             "processing": true, // Optional: for large datasets
             "deferRender": true, // Improves performance by rendering rows only when needed
             "order": [
