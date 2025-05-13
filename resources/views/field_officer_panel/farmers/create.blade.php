a1a23a3d52c6c827d75850e60a55081df3bbe904
Mohammad Azhar <90447167+MohammarAzher@users.noreply.github.com>
2025-05-13 10:44:49 +0500

    up

--- resources/views/field_officer_panel/farmers/create.blade.php
+++ resources/views/field_officer_panel/farmers/create.blade.php
@@ -333,12 +333,12 @@
                                             <label class="form-label">CNIC Issue Date.: <span class="text-danger">*</span></label>
                                             <input type="text" id="cnic_issue_date" name="cnic_issue_date" class="form-control" value="@if(isset($data)) {{ $data->cnic_issue_date ? \Carbon\Carbon::parse($data->cnic_issue_date)->format('d-m-Y') : '' }} @endif"   data-inputmask="'mask': '99-99-9999'" placeholder="DD-MM-YYYY"   >
                                         </div>
-                                        <div class="mb-6 col-md-2 py-2 cnic_expiry_date_div">
+                                        <div class="mb-6 col-md-2 py-2 cnic_expiry_date_div"    style=" @if(isset($data) && $data->cnic_expiry_date == '')  display:none; @endif ">
                                             <label class="form-label">CNIC Expiry Date.: <span class="text-danger">*</span></label>
                                             <input type="text" id="cnic_expiry_date" name="cnic_expiry_date" class="form-control" value="@if(isset($data))  {{ $data->cnic_expiry_date ? \Carbon\Carbon::parse($data->cnic_expiry_date)->format('d-m-Y') : '' }} @endif"   data-inputmask="'mask': '99-99-9999'" placeholder="DD-MM-YYYY"   >
                                         </div>
                                         <div class="mb-6 col-md-6 py-2">
-                                            <label class="form-label">Q4. Mobile No.: <span class="text-danger">*</span></label>
+                                            <label class="form-label" >Q4. Mobile No.: <span class="text-danger">*</span></label>
                                             <input type="text" id="mobile" name="mobile" class="form-control"
                                             value="{{ $data->mobile ?? '' }}"
                                             data-inputmask="'mask': '0399-9999999', 'greedy': false"
