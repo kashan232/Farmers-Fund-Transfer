9625a52c3c536b7964f9884013299abd99072d78
Mohammad Azhar <90447167+MohammarAzher@users.noreply.github.com>
2025-05-15 12:26:34 +0500

    up

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -197,7 +197,7 @@ class HomeController extends Controller
                     'verified_by_dd',
                 ];
 
-                $data = Tehsil::where('district',$user->district)->withCount([
+                $data = Tehsil::whereIn('district',json_decode($user->district))->withCount([
                     // Total farmers per district
                     'farmers as total' => function ($q) {
                         // No filter; count all
