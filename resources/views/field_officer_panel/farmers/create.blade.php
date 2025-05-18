a6d00263c66eb2a37c07aa325fc32c8afeaf5e51
Mohammad Azhar <90447167+MohammarAzher@users.noreply.github.com>
2025-05-18 20:59:11 +0500

    up

--- resources/views/field_officer_panel/farmers/create.blade.php
+++ resources/views/field_officer_panel/farmers/create.blade.php
@@ -330,7 +330,7 @@
 
 
                                         @php
-                                            function isValidDate($date, $format = 'd-m-Y') {
+                                            function isValidDate($date, $format = 'Y-m-d') {
                                                 $d = DateTime::createFromFormat($format, $date);
                                                 return $d && $d->format($format) === $date;
                                             }
