1435355dc6ffcb707fc0d4cc2895cfb6d7e4e000
Mohammad Azhar <90447167+MohammarAzher@users.noreply.github.com>
2025-06-18 17:43:04 +0500

    up

--- resources/views/pd_officer_panel/field_officer_list.blade.php
+++ resources/views/pd_officer_panel/field_officer_list.blade.php
@@ -39,7 +39,7 @@
                 }
                 if($users[0]->usertype == 'Agri_Officer'){
                     $groupedData = $users->groupBy('tehsil')->map(function ($group) {
-                        return $group->sum('forwarded_to_dd');
+                        return $group->sum('farmerCount');
                     });
                 }
 
