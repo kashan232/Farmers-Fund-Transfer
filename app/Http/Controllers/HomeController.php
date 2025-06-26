4c2ab37a0801d37490f3f306e00a3f4f3f7e0c21
Mohammad Azhar <90447167+MohammarAzher@users.noreply.github.com>
2025-06-26 13:04:52 +0500

    up

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -671,15 +671,7 @@ class HomeController extends Controller
                 $Unverifiedfarmeragiruser = LandRevenueFarmerRegistation::where('district', $user->district)
                 ->where('tehsil', $user->tehsil)
                 ->whereIn('tappa', json_decode($user->tappas))
-                ->where(function ($query) use ($user) {
-                    $query->where(function ($q) use ($user) {
-                        $q->where('user_type', 'Field_Officer')
-                        ->where('user_id', $user->id);
-                    })->orWhere(function ($q) {
-                        $q->where('user_type', 'Online')
-                        ->whereNull('user_id');
-                    });
-                })
+              
                  ->whereNull('verification_status')
                 ->count();
 
@@ -687,7 +679,7 @@ class HomeController extends Controller
                 $onProcessFarmer = LandRevenueFarmerRegistation::where('district', $user->district)
                 ->where('tehsil', $user->tehsil)
                 ->whereIn('tappa', json_decode($user->tappas))
-              
+
                 ->whereIn('verification_status', [
                     'rejected_by_lrd',
                     'rejected_by_ao',
@@ -701,15 +693,7 @@ class HomeController extends Controller
                 $Verifiedfarmeragiruser = LandRevenueFarmerRegistation::where('district', $user->district)
                 ->where('tehsil', $user->tehsil)
                 ->whereIn('tappa', json_decode($user->tappas))
-                ->where(function ($query) use ($user) {
-                    $query->where(function ($q) use ($user) {
-                        $q->where('user_type', 'Field_Officer')
-                        ->where('user_id', $user->id);
-                    })->orWhere(function ($q) {
-                        $q->where('user_type', 'Online')
-                        ->whereNull('user_id');
-                    });
-                })
+
                 ->where('verification_status' , 'verified_by_lrd')
                 ->count();
 
