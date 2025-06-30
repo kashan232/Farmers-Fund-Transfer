e3effb37b2a1a47ae41dbeb5a1fd712d3e07add1
Mohammad Azhar <90447167+MohammarAzher@users.noreply.github.com>
2025-06-30 14:22:55 +0500

    up

--- resources/views/pd_officer_panel/field_officer_list.blade.php
+++ resources/views/pd_officer_panel/field_officer_list.blade.php
@@ -284,7 +284,7 @@
                                     <th>S#</th>
                                     <th>Tehsil</th>
                                     <th>Total Farmers</th>
-                                    {{-- <th>Verified By LRD</th> --}}
+                                    <th>Verified By LRD</th>
                                     <th>In-Process</th>
                                     <th>Rejected</th>
                                     <th>Pending</th>
@@ -302,7 +302,7 @@
 
                                         <td>{{ $groupedData_farmers_count[$tehsil] ?? 0 }}</td>
 
-                                        {{-- <td>{{ $groupedData_verified_by_lrd[$tehsil] ?? 0 }}</td> --}}
+                                        <td>{{ $groupedData_verified_by_lrd[$tehsil] ?? 0 }}</td>
 
 
                                         <td>{{ $groupedData_forwarded_to_dd[$tehsil] ?? 0 }}</td>
@@ -650,7 +650,7 @@
 
 
                                                                 @if($user->usertype == 'Agri_Officer'  ) <span style="font-size:12px">
-                                                                    {{-- Verified By LRD  = <span style="font-size:15px"> <a href="{{ route('dg.farmers', ['user_id' => $user->id, 'farmer_type_status_by_ao' => 'verified_by_lrd']) }}">  {{ $user->verified_by_lrd }} </a> </span> <br> --}}
+                                                                    Verified By LRD  = <span style="font-size:15px"> <a href="{{ route('dg.farmers', ['user_id' => $user->id, 'farmer_type_status_by_ao' => 'verified_by_lrd']) }}">  {{ $user->verified_by_lrd }} </a> </span> <br>
 
                                                                     In-Process  = <span style="font-size:15px"> <a href="{{ route('dg.farmers', ['user_id' => $user->id, 'farmer_type_status_by_ao' => 'in-Process']) }}">  {{ $user->forwarded_to_dd }} </a> </span> <br>
                                                                     Rejected = <span style="font-size:15px"> <a href="{{ route('dg.farmers', ['user_id' => $user->id, 'farmer_type_status_by_ao' => 'rejected']) }}"> {{ $user->rejected_by_ao }} </a> </span> <br>
