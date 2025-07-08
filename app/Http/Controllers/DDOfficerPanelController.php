c58b6d4f79f1a673d476087ab846d911995d05fe
Mohammad Azhar <90447167+MohammarAzher@users.noreply.github.com>
2025-07-09 01:54:40 +0500

    up

--- app/Http/Controllers/DDOfficerPanelController.php
+++ app/Http/Controllers/DDOfficerPanelController.php
@@ -49,13 +49,16 @@ class DDOfficerPanelController extends Controller
              $query->where('district', $user->district)
             ->where('tehsil', $user->tehsil)
             ->whereIn('tappa', $tappas)
-            ->whereIn('verification_status', [
-                'verified_by_fa',
-                'verified_by_ao',
-                'verified_by_lrd',
-                'rejected_by_ao',
-                'rejected_by_lrd',
-            ]);
+             ->where(function ($query) {
+                $query->whereIn('verification_status', [
+                    'verified_by_fa',
+                    'verified_by_ao',
+                    'verified_by_lrd',
+                    'rejected_by_fa',
+                    'rejected_by_ao',
+                    'rejected_by_lrd',
+                ])->orWhereNull('verification_status');
+            });
 
 
         }
