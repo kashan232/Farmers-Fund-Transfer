8169312f5e69c2e80f67ca479d3eae464713ff5b
Mohammad Azhar <90447167+MohammarAzher@users.noreply.github.com>
2025-07-20 13:45:33 +0500

    up

--- app/Http/Controllers/DGOfficerPanelController.php
+++ app/Http/Controllers/DGOfficerPanelController.php
@@ -28,7 +28,7 @@ public function excelExport(Request $request)
         $fileName = 'farmers_export_all.csv';
     }
 
-
+    dd($request->all());
 
     $farmers = LandRevenueFarmerRegistation::with('branch')
         ->when($request->search, fn($q) => $q->where('name', 'like', "%{$request->search}%"))
