9b8982a088b10842951997778ac7a61608da34da
Mohammad Azhar <90447167+MohammarAzher@users.noreply.github.com>
2025-07-29 13:38:48 +0500

    up

--- app/Http/Controllers/DDOfficerPanelController.php
+++ app/Http/Controllers/DDOfficerPanelController.php
@@ -297,7 +297,7 @@ class DDOfficerPanelController extends Controller
 
 
         $totalFarmers = (clone $query)->count();
-        $farmers = $query->paginate(150)->appends($req->all());
+        $farmers = $query->paginate(5000)->appends($req->all());
         return view('dd_officer_panel.farmers.index',['farmers' => $farmers, 'totalFarmers' => $totalFarmers]);
 
 
