11bbbbc0e79adb2ad80c5ab0d650dfcd13ef1738
Mohammad Azhar <90447167+MohammarAzher@users.noreply.github.com>
2025-08-08 15:09:37 +0500

    up

--- app/Imports/FarmersImport.php
+++ app/Imports/FarmersImport.php
@@ -2,7 +2,7 @@
 
 namespace App\Imports;
 
-use App\Models\LandRevenueFarmerRegistation;
+use App\Models\HardCopyFarmer;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Illuminate\Support\Facades\Auth;
 use Maatwebsite\Excel\Concerns\WithStartRow;
@@ -24,83 +24,64 @@ class FarmersImport implements ToModel, WithStartRow
     public function model(array $row)
     {
         try{
-            if (count($row) !== 8) {
-                throw new \Exception("Excel Invalid...");
-            }
+            // if (count($row) !== 8) {
+            //     throw new \Exception("Excel Invalid...");
+            // }
 
 
-                if($row[4] == null)
-                {
-                    throw new \Exception("District Field Empty Row: " .$this->rowNumber);
+
+
+
+                // Example usage
+                $cnic_issue_date = $row[5];
+                if (is_numeric($cnic_issue_date)) {
+                    $cnic_issue_date = $this->excelDateToDate($cnic_issue_date);
                 }
-                if($row[5] == null)
-                {
-                    throw new \Exception("Tehsil Field Empty Row: " .$this->rowNumber);
+                else{
+                    $cnic_issue_date = $row[5];
                 }
-                $this->rowNumber++;
 
 
-                return new LandRevenueFarmerRegistation([
+                // Example usage
+                $cnic_expiry_date = $row[5];
+                if (is_numeric($cnic_expiry_date)) {
+                    $cnic_expiry_date = $this->excelDateToDate($cnic_expiry_date);
+                }
+                else{
+                    $cnic_expiry_date = $row[5];
+                }
+
+
+                // Example usage
+                $date_of_birth = $row[5];
+                if (is_numeric($date_of_birth)) {
+                    $date_of_birth = $this->excelDateToDate($date_of_birth);
+                }
+                else{
+                    $date_of_birth = $row[5];
+                }
+
+                return new HardCopyFarmer([
 
                     // $data['admin_or_user_id'] = Auth::id();
                     // $data['land_emp_id'] = Auth()->user()->user_id;
                     // $data['land_emp_name'] = Auth()->user()->name;
 
-                    'admin_or_user_id' => Auth::id(),
-                    'land_emp_id' => Auth()->user()->user_id,
-                    'land_emp_name' => Auth()->user()->name,
-                    'name' => $row[0],
-                    'father_name' => $row[1],
-                    'cnic' => $row[2],
-                    'mobile' => $row[3],
-                    'district' => $row[4],
-                    'tehsil' => $row[5],
-                    'uc' => $row[6],
-                    'tappa' => $row[7],
-                    'dah' => $row[8],
-                    'village' => $row[9],
-                    'gender' => $row[10],
-                    'house_type' => $row[11],
-                    'owner_type' => $row[12],
-                    'female_children_under16' => $row[13],
-                    'female_Adults_above16' => $row[14],
-                    'male_children_under16' => $row[15],
-                    'male_Adults_above16' => $row[16],
-                    'total_landing_acre' => $row[17],
-                    'total_area_with_hari' => $row[18],
-                    'total_area_cultivated_land' => $row[19],
-                    'total_fallow_land' => $row[20],
-                    'title_name' => $this->StringToJson($row[21]),
-                    'title_cnic' => $this->StringToJson($row[22]),
-                    'title_number' => $this->StringToJson($row[23]),
-                    'title_area' => $this->StringToJson($row[24]),
-                    'crops' => $this->StringToJson($row[25]),
-                    'crop_area' => $this->StringToJson($row[26]),
-                    'crop_average_yeild' => $this->StringToJson($row[27]),
-                    'physical_asset_item' => $this->StringToJson($row[28]),
-                    'animal_name' => $this->StringToJson($row[29]),
-                    'animal_qty' => $this->StringToJson($row[30]),
-                    'source_of_irrigation' => $row[31],
-                    'source_of_irrigation_engery' => $row[32],
-                    'area_length' => $row[33],
-                    'line_status' => $row[34],
-                    'lined_length' => $row[35],
-                    'total_command_area' => $row[36],
-                    'account_title' => $row[37],
-                    'account_no' => $row[38],
-                    'bank_name' => $row[39],
-                    'branch_name' => $row[40],
-                    'IBAN_number' => $row[41],
-                    'branch_code' => $row[42],
-                    'front_id_card' => $row[43],
-                    'back_id_card' => $row[44],
-                    'upload_land_proof' => $row[45],
-                    'upload_other_attach' => $row[46],
-                    'upload_farmer_pic' => $row[47],
-                    'upload_cheque_pic' => $row[48],
-                    'verification_status' => null,
-                    'declined_reason' => null,
-                    'verification_by' => null,
+                    'name' => $row[1],
+                    'father_name' => $row[2],
+                    'mother_maiden_name' => $row[3],
+                    'cnic' => $row[4],
+                    'cnic_issue_date' => $cnic_issue_date,
+                    'cnic_expiry_date' => $cnic_expiry_date,
+                    'date_of_birth' => $date_of_birth,
+                    'gender' => $row[9],
+                    'correspondence_address' => $row[11],
+                    'permanent_address' => $row[11],
+                    'district' => $row[13],
+                    'mobile' => $row[14],
+                    'survey_no' => $row[15],
+                    'total_landing_acre' => $row[16],
+                    'total_area_cultivated_land' => $row[17],
                 ]);
                 $rowNumber++;
                // dd($this->StringToJson($row[28]));
@@ -113,6 +94,13 @@ class FarmersImport implements ToModel, WithStartRow
 
 
     }
+
+
+     function excelDateToDate($serial) {
+        $unixDate = ($serial - 25569) * 86400; // Convert Excel serial date to Unix timestamp
+        return date('Y-m-d', $unixDate); // Format it to '22-07-2024'
+    }
+
 
     private function StringToJson($e){
         $array = explode(",", $e);
