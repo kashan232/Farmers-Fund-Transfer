158bc072607231dc910f18d8c30501ae1d4d25bc
Mohammad Azhar <90447167+MohammarAzher@users.noreply.github.com>
2025-08-31 23:14:19 +0500

    up

--- resources/views/backend_registration_form.blade.php
+++ resources/views/backend_registration_form.blade.php
@@ -401,8 +401,9 @@ https://cdn.jsdelivr.net/npm/sweetalert2@11.14.5/dist/sweetalert2.min.css
                     @php
                         $filters = request()->only(['search']);
                     @endphp
-                    <form action="" class="d-flex col-4" style="margin-left: 4%;">
-                        <input type="text" value="{{ $filters['search'] ?? 'Enter CNIC without hyphen/dashes' }}" class="form-control" style="margin-right: 2%" name="search" id="search"  placeholder="Enter CNIC without hyphen/dashes">
+                    <label for="">Enter CNIC without hyphen/dashes</label>
+                    <form action="" class="d-flex col-3" style="margin-left: 4%;">
+                        <input type="text" value="{{ $filters['search'] ?? '' }}" class="form-control" style="margin-right: 2%" name="search" id="search" oninput="this.value = this.value.replace(/[^0-9]/g, '').slice(0, 13)" placeholder="CNIC">
                         <button type="submit" class="btn btn-primary">Search</button>
                     </form>
 
